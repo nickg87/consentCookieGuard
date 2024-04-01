849e8bcca7cf89662462b4b5f05be8a739d6a9a0
nickg87 <guliman.nicu@gmail.com>
2024-04-01 15:37:37 +0300

    gitignore update and npm build

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     entry: './consent.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, '..', 'app', 'js', 'dist')
+        path: path.resolve(__dirname, './', 'dist')
     },
     module: {
         rules: [
