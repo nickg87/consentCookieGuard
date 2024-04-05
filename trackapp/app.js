0be9fa07edbac79cf92db7cdd420b34ffa1b7197
nickg87 <guliman.nicu@gmail.com>
2024-04-05 16:00:17 +0300

    rollback

--- trackapp/app.js
+++ trackapp/app.js
@@ -18,7 +18,8 @@ app.get('/pixel.png', (req, res) => {
     res.sendFile('pixel.png', { root: __dirname });
 });
 
-const PORT = 80;
+// Start the server
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
