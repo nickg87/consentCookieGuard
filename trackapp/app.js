86bec5ae155ee526b814a798cf38b25e6079110b
nickg87 <guliman.nicu@gmail.com>
2024-04-05 15:52:07 +0300

    try port 80

--- trackapp/app.js
+++ trackapp/app.js
@@ -18,8 +18,7 @@ app.get('/pixel.png', (req, res) => {
     res.sendFile('pixel.png', { root: __dirname });
 });
 
-// Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = 80;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
