b18392a86aa2cbffed4917c315eb491672b5fda4
nickg87 <guliman.nicu@gmail.com>
2024-04-09 16:51:31 +0300

    added build-dev (uses dev.bundle.js)

--- webpack.config.js
+++ webpack.config.js
@@ -3,36 +3,40 @@ const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
-module.exports = {
-    mode: 'production', // Change mode to production for optimized output
-    entry: './consent.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, './', 'dist')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader']
-            }
+module.exports = (env) => {
+    const isDevelopment = env && env.development === 'true';
+
+    return {
+        mode: isDevelopment ? 'development' : 'production',
+        entry: './consent.js',
+        output: {
+            filename: isDevelopment ? 'dev.bundle.js' : 'bundle.js', // Include a prefix or suffix for the development bundle
+            path: path.resolve(__dirname, './', 'dist')
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use: [MiniCssExtractPlugin.loader, 'css-loader']
+                }
+            ]
+        },
+        optimization: {
+            minimize: true,
+            minimizer: [
+                new TerserPlugin({
+                    terserOptions: {
+                        // Keep function names readable for debugging
+                        mangle: false
+                    },
+                }),
+                new CssMinimizerPlugin() // Add CSS minification plugin
+            ]
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: 'styles.css' // Output CSS filename
+            })
         ]
-    },
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new TerserPlugin({
-                terserOptions: {
-                    // Keep function names readable for debugging
-                    mangle: false
-                },
-            }),
-            new CssMinimizerPlugin() // Add CSS minification plugin
-        ]
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: 'styles.css' // Output CSS filename
-        })
-    ]
+    };
 };
