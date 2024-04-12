ecac00349b6c8f3885f001f05ddd3c297695bca5
nickg87 <guliman.nicu@gmail.com>
2024-04-12 11:46:53 +0300

    fix typo

--- consent.js
+++ consent.js
@@ -478,7 +478,7 @@ window.cg__displayCookieConsentModal = () => {
                     <div id="___cookieConsent__ConsentTabText">
                         <div style="display: block">
                         ${consimtamantText?.content.map(paragraph => `<p>${paragraph}</p>`).join('')}
-                        ${CUSTOM_COOKIE_LINK ? `Citește mai multe: <a href="${CUSTOM_COOKIE_LINK}" title="Politica c0okies" target="_blank">aici</a>` : ''}
+                        ${CUSTOM_COOKIE_LINK ? `Citește mai multe <a href="${CUSTOM_COOKIE_LINK}" title="Politica c0okies" target="_blank">aici</a>` : ''}
                         </div>
                     </div>
                     <p style="margin-top: 5px">Selectează din lista de mai jos:</p>
